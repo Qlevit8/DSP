58bcc357b4467055153251ae3aad21e821908936
Михаил Мазенко <mazenko.m01@gmail.com>
2023-09-14 01:31:01 +0300

    Add project files.

---
+++ InterfaceElements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace DSPLabs;
+
+public class InterfaceElements
+{
+    public static int CornerRadiusValue { get; private set; } = 15;
+    public static int HalfCornerRadiusValue { get; private set; } = CornerRadiusValue / 2;
+    public static SolidColorBrush ExitButtonColor { get; private set; } = new SolidColorBrush(Color.FromRgb(239, 59, 62));
+    public static SolidColorBrush MinimizeButtonColor { get; private set; } = new SolidColorBrush(Color.FromRgb(241, 222, 21));
+    public static SolidColorBrush ButtonInactiveColor { get; private set; } = new SolidColorBrush(Color.FromRgb(166, 8, 8));
+    public static int FontSize { get; private set; } = 14;
+    public static FontFamily FontFamily { get; private set; } = new FontFamily("Bahnschrift Light");
+    public static SolidColorBrush FontColor { get; private set; } = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace DSPLabs
+{
+    public partial class MainWindow : Window
+    {
+        public InterfaceElements Interface { get; private set; }
+        private bool isDragging = false;
+        private Point startPoint;
+        public MainWindow()
+        {
+            Interface = new InterfaceElements();
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void WindowMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                startPoint = e.GetPosition(this);
+                isDragging = true;
+            }
+        }
+
+        private void WindowMouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point endPoint = e.GetPosition(this);
+                double offsetX = endPoint.X - startPoint.X;
+                double offsetY = endPoint.Y - startPoint.Y;
+
+                Left += offsetX;
+                Top += offsetY;
+            }
+        }
+
+        private void WindowMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                isDragging = false;
+        }
+        private void Exit(object sender, RoutedEventArgs e) =>
+            Application.Current.Shutdown();
+
+        private void Minimize(object sender, RoutedEventArgs e) =>
+            WindowState = WindowState.Minimized;
+    }
+}
